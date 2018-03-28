0f40120f5616883b7051e66829988b0f55cdb3e3
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-28 21:13:07 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "Black", "Friendly" True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 11, "M", "Brown", "Aggressive" True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "White", "Sweet" True);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 30, "M", "Black", "Sleepy" True);
